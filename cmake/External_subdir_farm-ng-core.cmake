24a4b4cc08e51d28719acc0e82c06a0cb481d982
Ethan Rublee <ethan.rublee@gmail.com>
2022-10-02 16:57:24 -0700

    farm_ng_cmake_DIR.

--- cmake/External_subdir_farm-ng-core.cmake
+++ cmake/External_subdir_farm-ng-core.cmake
@@ -3,6 +3,8 @@ ExternalProject_Add(farm-ng-core
     PREFIX ${farm_ng_EXT_PREFIX}
     SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/farm-ng-core
     BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/farm-ng-core-build
-    CMAKE_ARGS ${farm_ng_DEFAULT_ARGS} -DBUILD_FARM_NG_PROTOS=On
+    CMAKE_ARGS ${farm_ng_DEFAULT_ARGS} \
+        -DBUILD_FARM_NG_PROTOS=On \
+        -Dfarm_ng_cmake_DIR=${farm_ng_cmake_DIR}
     TEST_BEFORE_INSTALL ON
 )
