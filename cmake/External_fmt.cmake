23dd06e5478318bbcda785101b1511c7185b3e4f
Steven Lovegrove <stevenlovegrove@gmail.com>
2022-10-27 13:19:04 -0700

    Upgrade to more recent fmt.

--- cmake/External_fmt.cmake
+++ cmake/External_fmt.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(fmt
     GIT_REPOSITORY  "https://github.com/fmtlib/fmt.git"
-    GIT_TAG "8.1.1"
+    GIT_TAG "9.1.0"
     PREFIX ${farm_ng_EXT_PREFIX}
     CMAKE_ARGS
     ${farm_ng_DEFAULT_ARGS}
