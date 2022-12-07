ae21a4f8214b6a3dc248422928f1571cf8387a2e
Hauke Strasdat <strasdat@gmail.com>
2022-12-06 23:16:57 -0800

    c++17 + concepts

--- cmake/farm_ng_module.cmake
+++ cmake/farm_ng_module.cmake
@@ -19,12 +19,17 @@ macro(farm_ng_module)
 
   set(PKG_CONFIG_USE_CMAKE_PREFIX_PATH ON)
   set(BUILD_SHARED_LIBS ON)
-  set(CMAKE_CXX_STANDARD 17)
   set(CMAKE_CXX_STANDARD_REQUIRED ON)
+  
+  # Coarsely, the c++ standard used is c++17 plus concepts from c++20.
   if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
+    # For clang, c++20 is enabled, but only concepts shall be used from the features newly added.
+    set(CMAKE_CXX_STANDARD 20)  
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror -Wall -Wextra -Wsign-compare -Wno-unused-parameter -Wno-unused-variable -Wno-unused-function -Wreorder-ctor -Wreorder-init-list")
   elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror -Wall -Wextra -Wno-unused-parameter -Wno-unused-but-set-variable -Wno-unused-variable -Wno-unused-function -Wno-maybe-uninitialized")
+    set(CMAKE_CXX_STANDARD 17)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fconcepts") 
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror -Wall -Wextra -Wno-missing-field-initializers -Wno-unused-parameter -Wno-unused-but-set-variable -Wno-unused-variable -Wno-unused-function -Wno-maybe-uninitialized")
   endif()
 
   set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
