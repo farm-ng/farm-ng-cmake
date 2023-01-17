ebc060562e6124b37a8af379dd6764b6abd326f7
Ian Sherman <ian.j.sherman@gmail.com>
2023-01-17 14:07:51 -0800

    First pass at release.yml action

---
+++ scripts/install_deps_mac.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -x # echo on
+set -e # exit on error
+
+brew install --verbose \
+    assimp \
+    boost \
+    catch2 \
+    ccache \
+    ceres-solver \
+    ffmpeg \
+    glew \
+    glog \
+    grpc \
+    libjpeg \
+    libpng \
+    libtiff \
+    lz4 \
+    opencv \
+    openexr \
+    openssl \
+    pre-commit \
+    protobuf \
+    zstd
---
+++ scripts/install_deps_ubuntu.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -x # echo on
+set -e # exit on error
+
+sudo apt-get -qq update
+
+# Please keep this list sorted.
+sudo apt-get -y install \
+     ccache \
+     clang \
+     clang-tidy \
+     cmake \
+     gfortran \
+     libassimp-dev \
+     libatlas-base-dev \
+     libavcodec-dev \
+     libavdevice-dev \
+     libavformat-dev \
+     libavutil-dev \
+     libboost-coroutine-dev \
+     libc++-dev \
+     libcrypto++-dev \
+     libegl1-mesa-dev \
+     libglew-dev \
+     libgoogle-glog-dev \
+     libgrpc-dev \
+     libgrpc++-dev \
+     libgtest-dev \
+     libopencv-dev \
+     libprotobuf-dev \
+     libssl-dev \
+     libsuitesparse-dev \
+     libswscale-dev \
+     libtiff-dev \
+     protobuf-compiler \
+     protobuf-compiler-grpc \
+     && sudo apt-get clean
+
+
+ls /usr/lib/x86_64-linux-gnu/libgrpc++*
