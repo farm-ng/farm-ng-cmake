a73be34dd1d268bd812a86a5af614e83ed879570
Ethan Rublee <ethan.rublee@gmail.com>
2023-01-17 18:43:27 -0800

    Remove commented packages.

--- scripts/install_deps_ubuntu.sh
+++ scripts/install_deps_ubuntu.sh
@@ -18,24 +18,15 @@ sudo apt-get -y install \
      libavdevice-dev \
      libavformat-dev \
      libavutil-dev \
-     # libboost-coroutine-dev \
      libc++-dev \
      libcrypto++-dev \
      libegl1-mesa-dev \
      libglew-dev \
      libgoogle-glog-dev \
-     # libgrpc-dev \
-     # libgrpc++-dev \
      libgtest-dev \
      libopencv-dev \
-     # libprotobuf-dev \
      libssl-dev \
      libsuitesparse-dev \
      libswscale-dev \
      libtiff-dev \
-     # protobuf-compiler \
-     # protobuf-compiler-grpc \
      && sudo apt-get clean
-
-
-# ls /usr/lib/x86_64-linux-gnu/libgrpc++*
