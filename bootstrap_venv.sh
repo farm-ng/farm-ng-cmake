a79922355005b348b78ca962de4d90995b59f29e
Steven Lovegrove <stevenlovegrove@gmail.com>
2023-01-24 12:20:39 -0800

    * Fix issue around use of SHA's instead of tags for ExternalProject_Add see: https://gitlab.kitware.com/cmake/cmake/-/issues/17770 for discussion * use source instead of execution to ensure venv is on path to favor pip installed cmake. * Add tar to .gitignore to prevent accidental commit.

--- bootstrap_venv.sh
+++ bootstrap_venv.sh
@@ -2,17 +2,15 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 python3 -m venv venv
-. venv/bin/activate
+source venv/bin/activate
 pip install -U pip
 pip install -U wheel
 pip install -U cmake ninja
 
-mkdir -p build.venv
-cd build.venv
-cmake \
+cmake -B build.venv \
     -DFARM_NG_DEV_BUILD=On \
     -Dfarm_ng_INSTALL_PREFIX=$DIR/venv/prefix \
     -G Ninja \
     $DIR/.
 
-ninja -v
+cmake --build build.venv
