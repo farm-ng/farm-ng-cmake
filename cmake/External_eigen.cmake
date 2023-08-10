98b05101d842baf002cb983aa3ed3f1610c89090
Ian Sherman <ian.j.sherman@gmail.com>
2023-08-10 10:19:09 -0700

    Fix Eigen GIT_SHALLOW argument

--- cmake/External_eigen.cmake
+++ cmake/External_eigen.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(eigen
     GIT_REPOSITORY  https://gitlab.com/libeigen/eigen.git
     GIT_TAG "bc57b926a0214340c67d91501f4672a014ecf8fe"
-    GIT_SHALLOW ON
+    GIT_SHALLOW OFF
     PREFIX ${farm_ng_EXT_PREFIX}
     CMAKE_ARGS
     ${farm_ng_DEFAULT_ARGS}
